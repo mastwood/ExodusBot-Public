3216b4af631a276819f8b135f1f61ecbaaea84a4
mastwood <mastwood101@gmail.com>
2018-07-03 21:54:50 -0400

    uploaded stuff

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('assert');
+var exodusBot = require('../lib');
+
+describe('exodusBot', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
